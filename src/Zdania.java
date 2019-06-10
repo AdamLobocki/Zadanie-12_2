884ee17e9ac632661418249dd3505ca9dfe15d2c
Adam <adamlobocki@gmail.com>
2019-06-11 00:56:35 +0200

    Zadanie zrobione

---
+++ src/Zdania.java
@@ -0,0 +1,11 @@
+public class Zdania {
+    //   Wyjątki dzielimy na KONTROLOWANE które trzeba obsłużyc i NIEKONTROLOWANE które można, ale nie trzeba pbsłużyć
+
+    //   Wyjątki NIEKONTROLOWANE dziedzicza po klasie RuntimeException
+
+    //   Wyjątki rzucamy korzystając z instrukcji THROW, jeśli rzucamy wyjątek kontrolowany musimy dodatkowo w sygnaturze
+    //   metody/konstruktora zadeklarowac wyjatek używajac słowa  THROWS
+
+    //   Do bloku try-cath możemy dodać dodatkowo blok FINALLY, który wykona się zawsze, niezaleznie, czy w bloku try wyjątek
+    //   wystąpił czy nie
+}
